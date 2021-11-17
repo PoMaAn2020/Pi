0ae85a136d2f5663fcdf2026630a9ad3422b2c60
PoAnMa2020 <poanma@yandex.ru>
2021-11-17 21:40:14 +0400

    “message”

---
+++ app/src/main/java/com/pomaan/pi/MainActivity.java
@@ -0,0 +1,9 @@
+package com.pomaan.pi;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void onClickResult(View view) {
+    }
+}
